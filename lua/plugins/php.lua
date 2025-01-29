47cf015247c435bf03ba64907abb8d130998910e
jngermon <jn.germon@gmail.com>
2025-01-29 09:13:38 +0100

    Use phpstan as php linter instead of phpcs

--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -1,5 +1,24 @@
 return {
     {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                --"phpcs",
+                "php-cs-fixer",
+                "phpstan",
+            },
+        },
+    },
+    {
+        "mfussenegger/nvim-lint",
+        optional = true,
+        opts = {
+            linters_by_ft = {
+                php = { "phpstan" },
+            },
+        },
+    },
+    {
         "stevearc/conform.nvim",
         optional = true,
         opts = {
