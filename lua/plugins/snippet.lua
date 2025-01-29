ccf62ed2ec2513fb316db757eef645c4a14a72c9
jngermon <jn.germon@gmail.com>
2025-01-29 17:04:18 +0100

    Fix snippets with Luasnip

--- lua/plugins/snippet.lua
+++ lua/plugins/snippet.lua
@@ -1,20 +1,7 @@
 return {
-    -- {
-    --     "L3MON4D3/LuaSnip",
-    --     -- follow latest release.
-    --     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    --     -- install jsregexp (optional!).
-    --     build = "make install_jsregexp",
-    -- },
-    -- {
-    --     "rafamadriz/friendly-snippets",
-    --     config = function()
-    --         vim.defer_fn(function()
-    --             print(vim.fn.stdpath("config"))
-    --         end, 500)
-    --         require("luasnip.loaders.from_vscode").lazy_load()
-    --         require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
-    --     end,
-    -- },
-    -- require("luasnip.loaders.from_snipmate").lazy_load(),
+    {
+        "saghen/blink.cmp",
+        version = "v0.10.0",
+    },
+    require("luasnip.loaders.from_snipmate").lazy_load(),
 }
