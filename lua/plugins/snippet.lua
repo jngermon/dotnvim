de506cf33972f3ee2e530d38f710cbe76a27b0bb
jngermon <jn.germon@gmail.com>
2025-02-05 22:44:10 +0100

    Add php snippets (vscode style)

--- lua/plugins/snippet.lua
+++ lua/plugins/snippet.lua
@@ -4,4 +4,5 @@ return {
         version = "v0.10.0",
     },
     require("luasnip.loaders.from_snipmate").lazy_load(),
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/my_snippets" }),
 }
