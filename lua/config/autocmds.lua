1e91b37649b8d58d26f69f6a53021eac46046715
jngermon <jn.germon@gmail.com>
2025-02-11 09:17:47 +0100

    Add git-conflict

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,3 +15,14 @@ vim.api.nvim_create_autocmd({ "DirChanged", "VimEnter" }, {
         end, 200)
     end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "GitConflictDetected",
+    callback = function()
+        vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
+        vim.keymap.set("n", "cww", function()
+            engage.conflict_buster()
+            create_buffer_local_mappings()
+        end)
+    end,
+})
