8e6b3ae23fef124a7ead2aaa4085cbedc3e63c4e
jngermon <jn.germon@gmail.com>
2025-03-18 14:55:04 +0100

    Free blink version

--- lua/plugins/snippet.lua
+++ lua/plugins/snippet.lua
@@ -1,7 +1,7 @@
 return {
     {
         "saghen/blink.cmp",
-        version = "v0.10.0",
+        -- version = "v0.10.0",
     },
     require("luasnip.loaders.from_snipmate").lazy_load(),
     require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/my_snippets" }),
