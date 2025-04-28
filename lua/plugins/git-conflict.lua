e45e0583ead51b8eb2e400acb89cce4555b5033b
jngermon <jn.germon@gmail.com>
2025-04-28 10:15:56 +0200

    Configure git-conflict

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,3 +1,23 @@
 return {
-    { "akinsho/git-conflict.nvim", version = "*", config = true },
+    {
+        "akinsho/git-conflict.nvim",
+        version = "*",
+        config = true,
+        init = function()
+            local wk = require("which-key")
+            wk.add({
+                {
+                    "<leader>gw",
+                    function()
+                        engage.conflict_buster()
+                        create_buffer_local_mappings()
+                    end,
+                    desc = "Launch conflict buster",
+                },
+            })
+        end,
+    },
+    {
+        "sindrets/diffview.nvim",
+    },
 }
