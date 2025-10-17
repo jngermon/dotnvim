bf693fbfa2e3063f69efcd035afebf35be3dc35a
jngermon <jn.germon@gmail.com>
2025-10-17 09:35:25 +0200

    Update keymap for new keyboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,18 +9,18 @@ map("n", "<leader>D", "<cmd>lua Snacks.dashboard.open()<cr>", { desc = "Dashboar
 map("n", "<C-u>", "<cmd>redo<cr>", { desc = "Redo", remap = true })
 
 -- For New Keyboard compatibility
-map("n", "<C-s>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-map("n", "<C-r>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-map("n", "<C-n>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-map("n", "<C-m>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+map("n", "<C-t>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-s>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-r>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-n>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
 map("n", "<S-LEFT>", "<cmd>BufferLineMovePrev<cr>", { desc = "Go to Left Buffer", remap = true })
 map("n", "<S-RIGHT>", "<cmd>BufferLineMoveNext<cr>", { desc = "Go to Right Buffer", remap = true })
 
 -- Move Lines
-map("n", "<A-r>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
-map("n", "<A-n>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
-map("i", "<A-r>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-map("i", "<A-n>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-map("v", "<A-r>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
-map("v", "<A-n>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+map("n", "<A-s>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map("n", "<A-r>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map("i", "<A-s>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-r>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-s>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-r>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
